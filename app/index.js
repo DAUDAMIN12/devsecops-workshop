b13d3c9019398bcf4fac08c06242ec42af6a7359
DAUDAMIN12 <daudamin151@gmail.com>
2026-02-02 19:17:51 +0500

    Add Node app Dockerfile and k8s manifests

--- app/index.js
+++ app/index.js
@@ -1,10 +1,4 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from DevSecOps Workshop 🚀");
-});
-
-app.listen(3000, () => {
-  console.log("App running on port 3000");
-});
+app.get("/", (req, res) => res.send("DevSecOps Workshop Working!"));
+app.listen(3000, () => console.log("Running on port 3000"));
